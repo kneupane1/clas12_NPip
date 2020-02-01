a20d17d264ef64d10c4ee9e3c0dcf76a9985e51a
Krishna Neupane <krishna_physics@yahoo.com>
2020-02-01 08:55:36 -0500

    small correction

---
+++ build/CMakeFiles/clas12_NPip.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clas12_NPip.dir/exe/clas12_NPip.cxx.o"
+  "clas12_NPip.pdb"
+  "clas12_NPip"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clas12_NPip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/clas12lib.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/krishna/Downloads/clas12_NPip/src/branches.cpp" "/home/krishna/Downloads/clas12_NPip/build/CMakeFiles/clas12lib.dir/src/branches.cpp.o"
+  "/home/krishna/Downloads/clas12_NPip/src/cuts.cpp" "/home/krishna/Downloads/clas12_NPip/build/CMakeFiles/clas12lib.dir/src/cuts.cpp.o"
+  "/home/krishna/Downloads/clas12_NPip/src/deltat.cpp" "/home/krishna/Downloads/clas12_NPip/build/CMakeFiles/clas12lib.dir/src/deltat.cpp.o"
+  "/home/krishna/Downloads/clas12_NPip/src/histogram.cpp" "/home/krishna/Downloads/clas12_NPip/build/CMakeFiles/clas12lib.dir/src/histogram.cpp.o"
+  "/home/krishna/Downloads/clas12_NPip/src/physics.cpp" "/home/krishna/Downloads/clas12_NPip/build/CMakeFiles/clas12lib.dir/src/physics.cpp.o"
+  "/home/krishna/Downloads/clas12_NPip/src/reaction.cpp" "/home/krishna/Downloads/clas12_NPip/build/CMakeFiles/clas12lib.dir/src/reaction.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/root/include"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
