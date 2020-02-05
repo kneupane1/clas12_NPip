8b0dfe739d3ed1a387a77b2e46398d292efb0192
Krishna Neupane <krishna_physics@yahoo.com>
2020-02-05 11:41:41 -0500

    updates

--- src/histogram.cpp
+++ src/histogram.cpp
@@ -44,9 +44,6 @@ void Histogram::makeHists() {
 
   for (short sec = 0; sec < NUM_SECTORS; sec++) {
 
-    MissingMass[sec] =
-        std::make_shared<TH1D>(Form("MM2_hist_sec_%d", sec),
-                               Form("MM2_hist_sec_%d", sec), bins, -4., 4.);
     for (short q2 = 0; q2 < NUM_Q2_BINS; q2++) {
       E_vs_theta_e_all_events[sec][q2] =
           std::make_shared<TH2D>(Form("E_vs_theta_e_all_events_%d_%d", sec, q2),
@@ -83,6 +80,10 @@ void Histogram::makeHists() {
       W_vs_q2_all_events[sec][q2] = std::make_shared<TH2D>(
           Form("WQ2_sec_%d_%d", sec, q2), Form("WQ2_sec_%d_%d", sec, q2), bins,
           zero, w_max, bins, zero, q2_max);
+
+      MissingMass[sec][q2] = std::make_shared<TH1D>(
+          Form("MM2_hist_sec_%d", sec), Form("MM2_hist_sec_%d", sec), bins,
+          -w_max, w_max);
     }
   }
 }
